14cdc0b226075560c4c3ccc503e0edf7f27641a4
Petrik Diak <diak@petrik.hu>
2025-05-09 12:56:56 +0200

    2. commit

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -9,4 +9,31 @@
 		
 
 	}
+
+	public Sorkerdes (string kerdes, List<string> valaszok, string helyesValasz)
+	{
+		Kerdes = kerdes;
+		Valaszok = valaszok;
+		HelyesValasz = helyesValasz;
+	}
+}
+
+public class Kerdes
+{
+	public string Kerdes { get; set; }
+	public List<string> Valaszok { get; set; }
+
+	public string HelyesValasz { get; set; }
+
+	public Kerdes(string kerdes, List<string> valaszok, string helyesValasz)
+	{
+		Kerdes = kerdes;
+		Valaszok = valaszok;
+		HelyesValasz = helyesValasz;
+	}
+}
+
+public class Kerdesek
+{
+	public <list>Kerdesek { get; set; }
 }
