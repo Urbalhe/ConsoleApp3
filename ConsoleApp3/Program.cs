0b65ca29335ea9495fe57525fddd1a67013c4c50
Petrik Diak <diak@petrik.hu>
2025-05-09 12:02:15 +0200

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp3
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Szia. üdvözöllek a legyen ön is milliomosban. Nyerj 50 milliót. ");
+		}
+	}
+}
